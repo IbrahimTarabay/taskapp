37ed6da3b0257d7307a62b579e6f567e3fa550ec
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-02-25 18:37:09 +0200

    hash the password

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 class UserModel extends \CodeIgniter\Model{
   protected $table = 'user';
-  protected $allowedFields = ['name','email'];
+  protected $allowedFields = ['name','email','password'];
   protected $returnType = 'App\Entities\User';
   protected $useTimestamps = true;
+  protected $beforeInsert = ['hashPassword'];
+  //This function will be called before the insert method is called
+  protected function hashPassword(array $data){
+    if(isset($data['data']['password'])){
+       $data['data']['password_hash']=password_hash($data['data']['password'],PASSWORD_DEFAULT);
+       unset($data['data']['password']);
+    }
+    return $data;
+  }
 }
