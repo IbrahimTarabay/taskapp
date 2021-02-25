fb9bca91989184276791e35148b5e5a43ecb8639
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-02-25 16:28:50 +0200

    add create method to insert new user

--- app/Controllers/Signup.php
+++ app/Controllers/Signup.php
@@ -6,4 +6,11 @@ class Signup extends BaseController{
   public function new(){
     return view("Signup/new");
   }
+
+  public function create(){
+     $user = new \App\Entities\User($this->request->getPost());
+     $model = new \App\Models\UserModel;
+     $model->insert($user);
+     echo "Signed up";
+  }
 }
---
+++ app/Models/UserModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+class UserModel extends \CodeIgniter\Model{
+  protected $table = 'user';
+  protected $allowedFields = ['name','email'];
+  protected $returnType = 'App\Entities\User';
+  protected $useTimestamps = true;
+}
