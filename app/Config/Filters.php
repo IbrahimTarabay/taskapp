a504650c81a992d63be22866075604a42592889a
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-02-27 17:08:52 +0200

    apply filter to certain routes to require the user not to be logged in

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -11,6 +11,7 @@ class Filters extends BaseConfig
 		'toolbar'  => \CodeIgniter\Filters\DebugToolbar::class,
 		'honeypot' => \CodeIgniter\Filters\Honeypot::class,
 		'login'    => \App\Filters\LoginFilter::class,
+		'guest'    => \App\Filters\GuestFilter::class,
 	];
 
 	// Always applied before every request
