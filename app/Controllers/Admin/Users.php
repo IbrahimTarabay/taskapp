a30f67501fe24b219ce20557b42b68fcdfdac35e
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-03-10 19:42:20 +0200

    insert a new user record

--- app/Controllers/Admin/Users.php
+++ app/Controllers/Admin/Users.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controllers\Admin;
 
+use App\Entities\User;
+
 class Users extends \App\Controllers\BaseController{
   private $model;
 
@@ -25,6 +27,28 @@ class Users extends \App\Controllers\BaseController{
     return view('Admin/Users/show',
      ['user'=>$user]);
   }
+
+  public function new(){
+    $user = new User;
+    return view('Admin/Users/new', 
+     ['user'=>$user]);
+  }
+
+  public function create(){
+    //$model = new \App\Models\TaskModel;
+
+    $user = new User($this->request->getPost());
+
+    if($this->model->insert($user)){
+      return redirect()->to("/admin/users/show/{$this->model->insertID}")
+      ->with('info','User created successfully');
+    }else{
+      return redirect()->back()
+      ->with('errors',$this->model->errors())
+      ->with('warning', 'Invalid data')
+      ->withInput();
+  }
+}
 
   private function getUserOr404($id){
 
