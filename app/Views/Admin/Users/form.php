97793f85623728ef120e9cd9195b05551ef9eb01
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-03-13 17:50:19 +0200

    add is_admin checkbox to the form

--- app/Views/Admin/Users/form.php
+++ app/Views/Admin/Users/form.php
@@ -20,3 +20,11 @@
    <label for="password_confirmation">Repeat Password</label>
    <input type="password" name="password_confirmation">
  </div>
+
+ <div>
+   <label for="is_admin">
+    <input type="checkbox" id="is_admin" name="is_admin" value="1"
+     <?php if(old('is_admin',$user->is_admin)): ?>checked<?php endif; ?>>
+     Administrator
+   </label>
+ </div>
