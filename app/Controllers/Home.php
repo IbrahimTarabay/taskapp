dc09f4d0bd3c4918fa2816cbe2754006cecc6097
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-03-13 23:30:52 +0200

    send email localhost

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,20 @@ class Home extends BaseController
 		return view("Home/index");
 	}
 
+	public function testEmail(){
+	  /*$email = service('email');
+	  $email->setTo('ibrahimesalah69@gmail.com.com');
+	  $email->setSubject('A test email');
+	  $email->setMessage('<h1>Hi Hema</h1>');*/
+
+	  $sub = "A test email";
+	  $msg = "<h1>Hi Hema</h1>";
+	  $rec = "ibrahimesalah69@gmail.com";
+    
+	  if(mail($rec,$sub,$msg)){
+        echo "Message Sent";
+	  }else{
+		 echo $email->printDebugger();
+	  }
+	}
 }
