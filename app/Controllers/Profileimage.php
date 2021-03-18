e2b96f49691579686613f5d7dac61a3da3039628
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-03-18 21:44:58 +0200

    Validate the uploaded file

--- app/Controllers/Profileimage.php
+++ app/Controllers/Profileimage.php
@@ -6,4 +6,18 @@ class Profileimage extends BaseController{
   public function edit(){
     return view('Profileimage/edit');
   }
+
+  public function update(){
+    $file = $this->request->getFile('image');
+
+    if(!$file->isValid()){
+      $error_code = $file->getError();
+
+      if($error_code == UPLOAD_ERR_NO_FILE){
+        return redirect()->back()
+        ->with('warning', 'No file selected');
+      }
+      throw new \RuntimeException($file->getErrorString(). " " .$error_code);
+    }
+  }
 }
