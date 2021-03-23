bc49696a75ce48c0245cad142fa86822c8e15189
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-03-23 17:12:49 +0200

    display the user's uploaded profile image in the show profile

--- app/Views/Profile/show.php
+++ app/Views/Profile/show.php
@@ -7,7 +7,7 @@
 <h1>Profile</h1>
 
 <?php if($user->profile_image): ?>
-
+    <img src="<?= site_url('/profile/image') ?>" width="200" height="200" alt="profile image">
 <?php else: ?>
 
  <img src="<?= site_url('/images/blank_profile.png') ?>" width="200" height="200" alt="profile image">
