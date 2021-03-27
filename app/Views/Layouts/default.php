f5b96d7189c8c63382d61496a1fbdad7a67fa977
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-03-27 17:18:07 +0200

    load a CSS framework in the HTML

--- app/Views/Layouts/default.php
+++ app/Views/Layouts/default.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $this->renderSection("title") ?></title>
     <link rel="stylesheet" type="text/css" href="<?= site_url('/css/auto-complete.css') ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
 </head>
 <body>
 
