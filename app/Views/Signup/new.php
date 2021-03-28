5dc094308baafe0890a0409499ad707d5ec3e018
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-03-28 18:41:23 +0200

    translate text in views:text in HTML elements

--- app/Views/Signup/new.php
+++ app/Views/Signup/new.php
@@ -1,6 +1,6 @@
 <?= $this->extend("Layouts/default") ?>
 
-<?= $this->section("title") ?>Signup<?= $this->endSection() ?>
+<?= $this->section("title") ?><?= lang('Signup.title') ?><?= $this->endSection() ?>
 
 <?= $this->section("content") ?>
   <h1 class="title"><?= lang('Signup.title') ?></h1>
@@ -18,28 +18,28 @@
 <?= form_open("/signup/create") ?>
 
  <div class="field">
-   <label class="label" for="name">Name</label>
+   <label class="label" for="name"><?= lang('Signup.name') ?></label>
    <div class="control">
     <input class="input" type="text" name="name" id="name" value="<?= old('name')?>">
    </div>
  </div>
 
  <div class="field">
-   <label class="label" for="email">email</label>
+   <label class="label" for="email"><?= lang('Signup.email') ?></label>
    <div class="control">
     <input class="input" type="text" name="email" id="email" value="<?= old('email')?>">
    </div> 
  </div>
 
  <div class="field">
-   <label class="label" for="password">Password</label>
+   <label class="label" for="password"><?= lang('Signup.password') ?></label>
    <div class="control">
     <input class="input" type="password" name="password">
    </div>
  </div>
 
  <div class="field">
-   <label class="label" for="password_confirmation">Repeat Password</label>
+   <label class="label" for="password_confirmation"><?= lang('Signup.repeat_password') ?></label>
    <div class="control">
     <input class="input" type="password" name="password_confirmation">
    </div>
@@ -47,11 +47,11 @@
 
 <div class="field is-grouped">
  <div class="control">
-  <button class="button is-primary">Sign up</button>
+  <button class="button is-primary"><?= lang('Signup.sign_up') ?></button>
  </div>
 
  <div class="control">
-  <a class="button" href="<?= site_url("/") ?>">Cancel</a>
+  <a class="button" href="<?= site_url("/") ?>"><?= lang('Signup.cancel') ?></a>
  </div>
 </div>
 
